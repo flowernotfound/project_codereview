4679f78396b9d6e1dbcdd74328f9ac12020e5c03
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-09-13 05:48:24 +0900

    add commentform

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,10 +1,12 @@
 <script>
-  import CodeForm from './CodeForm.svelte';
-  import CodeList from './CodeList.svelte';
+  import CodeForm from "./CodeForm.svelte";
+  import CodeList from "./CodeList.svelte";
+  import CommentForm from "./CommentForm.svelte"; 
 </script>
 
 <main>
-  <h1>Code Review Platform</h1>
+  <h1>42 Code Review Platform</h1>
   <CodeForm />
   <CodeList />
+  <CommentForm /> 
 </main>
