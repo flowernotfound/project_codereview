8678f77b970ed878a86bc309e42a6feb79d7bf9d
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-09-15 15:16:54 +0900

    fixed all

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,30 +1,14 @@
 <script>
-	export let name;
+  import CodeForm from "./CodeForm.svelte";
+  import CodeList from "./CodeList.svelte";
+  import CommentForm from "./CommentForm.svelte"; 
+  import CommentList from './CommentList.svelte'; // 追加
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>42 Code Review Platform</h1>
+  <CodeForm />
+  <CodeList />
+  <CommentForm /> 
+  <CommentList /> <!-- 追加 -->
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
