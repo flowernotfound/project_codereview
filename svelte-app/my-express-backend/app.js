6cd1bdf0d17d836d1885a0b5162ffe7043932e4b
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-09-15 15:10:41 +0900

    add modules

--- svelte-app/my-express-backend/app.js
+++ svelte-app/my-express-backend/app.js
@@ -1,79 +1,11 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const sqlite3 = require("sqlite3").verbose();
-const cors = require("cors");
 const app = express();
 const port = 3000;
-
-app.use(cors());
-
-// body-parserの設定
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// SQLiteの設定
-let db = new sqlite3.Database("./mydb.sqlite3");
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-// 新しく追加するコード投稿API
-app.post("/api/code", (req, res) => {
-  const code = req.body.code;
-  if (!code) {
-    return res.status(400).send("Code is required");
-  }
-
-  const sql = "INSERT INTO code_reviews (code) VALUES (?)";
-  db.run(sql, [code], function (err) {
-    if (err) {
-      return res.status(500).send(err.message);
-    }
-    res.send({ id: this.lastID });
-  });
-});
-
-// 修正後のコード
-app.get("/api/code", (req, res) => {
-  db.all(
-    "SELECT * FROM code_reviews ORDER BY created_at DESC",
-    [],
-    (err, rows) => {
-      if (err) {
-        return res.status(500).json({ error: err.message });
-      }
-      res.json(rows);
-    }
-  );
-});
-
-// 新しく追加するコメント投稿API
-app.post("/api/comments", (req, res) => {
-  const { code_review_id, comment } = req.body;
-  if (!code_review_id || !comment) {
-    return res.status(400).send("Code review ID and comment are required");
-  }
-
-  const sql = "INSERT INTO comments (code_review_id, comment) VALUES (?, ?)";
-  db.run(sql, [code_review_id, comment], function (err) {
-    if (err) {
-      return res.status(500).send(err.message);
-    }
-    res.send({ id: this.lastID });
-  });
-});
-
-app.get("/api/comments", (req, res) => {
-  db.all("SELECT * FROM comments ORDER BY created_at DESC", [], (err, rows) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    res.json(rows);
-  });
-});
-
-// サーバー起動
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,14 +1,30 @@
 <script>
-  import CodeForm from "./CodeForm.svelte";
-  import CodeList from "./CodeList.svelte";
-  import CommentForm from "./CommentForm.svelte"; 
-  import CommentList from './CommentList.svelte'; // 追加
+	export let name;
 </script>
 
 <main>
-  <h1>42 Code Review Platform</h1>
-  <CodeForm />
-  <CodeList />
-  <CommentForm /> 
-  <CommentList /> <!-- 追加 -->
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
